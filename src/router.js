d5510500e04c2efcc7c64a4697f18def0e881f24
Kai Yamada <gpioblink@aol.com>
2019-07-20 15:40:43 +0900

    add router

--- src/router.js
+++ src/router.js
@@ -20,6 +20,36 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/add-property',
+      name: 'add-property',
+      component: () => import('./views/AddProperty.vue')
+    },
+    {
+      path: '/borrowable-list',
+      name: 'borrowable-list',
+      component: () => import('./views/BorrowableList.vue')
+    },
+    {
+      path: '/borrowing-list',
+      name: 'add-property',
+      component: () => import('./views/BorrowingList.vue')
+    },
+    {
+      path: '/chose-borrowing',
+      name: 'chose-borrowing',
+      component: () => import('./views/ChoseBorrowing.vue')
+    },
+    {
+      path: '/login',
+      name: 'login-user',
+      component: () => import('./views/LoginUser.vue')
+    },
+    {
+      path: '/register',
+      name: 'register-new-user',
+      component: () => import('./views/RegisterNewUser.vue')
     }
   ]
 })
