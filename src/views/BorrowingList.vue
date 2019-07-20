2937be16c9b9ad90131cd23e7467a417ff7ce78d
i80486dx2 <i80486dx23419@gmail.com>
2019-07-21 04:11:04 +0900

    BrorrowingList & ChoseBorrowing View

--- src/views/BorrowingList.vue
+++ src/views/BorrowingList.vue
@@ -1,5 +1,39 @@
 <template>
-  <div class="about">
-    <h1>This is an borrowinglist page</h1>
+  <div id="prop">
+   <v-breadcrumbs :items="items" divider=">" light>
+   </v-breadcrumbs>
+    <v-layout align-center justify-center>
+      <v-flex md5 class="white">
+        <v-container style="position: relative;top: 20%;" class="text-md-center">
+          <v-card flat>
+            <v-card-title>
+              <h2>This is an borrowinglist page</h2>
+            </v-card-title>
+           
+          </v-card>
+        </v-container>
+      </v-flex>
+    </v-layout>
+    
   </div>
 </template>
+
+<script>
+  export default {
+    data: () => ({
+      items: [
+        {
+          text: 'ホーム',
+          disabled: false,
+          href: 'home'
+        },
+        {
+          text: '返す',
+          disabled: true,
+          href: 'borrowing-list'
+        },
+      ],
+    })
+  }
+    
+</script>
