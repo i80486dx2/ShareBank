678b6702cc0dcb6624d606bc3dd4cba566995034
濱口周仁 <hamaguchishuto@wlan-napt-003.u-aizu.ac.jp>
2019-07-21 00:27:21 +0900

    add ui

--- src/views/AddProperty.vue
+++ src/views/AddProperty.vue
@@ -1,35 +1,20 @@
 <template>
-  <div class="about">
-    <h1>This is an add property page</h1>
-    <v-btn primary large block v-on:click="pushProperty('テスト')">test</v-btn>
+  <div id="prop">
+    <v-form ref="form" v-model="valid" lazy-validation align-center>
+     <v-text-field
+      v-model="item"
+      :rules="propertyRules"
+      label="登録品名"
+      required
+     ></v-text-field>
+     <v-btn
+      :disabled="!valid"
+      @click="登録"
+      color="red"
+     >
+     登録
+     </v-btn>
+    
+    </v-form>
   </div>
 </template>
-
-<script>
-import firebase from 'firebase';
-export default {
-  name: 'AddProperty',
-  components: {
-  },
-  data () {
-    return {
-      db: firebase.firestore(),
-    }
-  },
-  methods: {
-      pushProperty(name) {
-        this.db.collection("properties").add({
-            name: name,
-            ownerId: firebase.auth().currentUser.uid,
-            ownerName: firebase.auth().currentUser.displayName
-        })
-        .then(function(docRef) {
-            console.log("Document written with ID: ", docRef.id);
-        })
-        .catch(function(error) {
-            console.error("Error adding document: ", error);
-        });
-      }
-  }
-}
-</script>
